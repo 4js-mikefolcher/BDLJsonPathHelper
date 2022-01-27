dd81c1f4216f5763f34eaf49d77779171760026e
Mike Folcher <mifo@4js.com>
2022-01-27 11:35:01 -0500

    Change jar-build.sh script to test after compile
    Add logic to the jar-build.sh script to test run the BDLJsonPath
    class after compiling but before the jar file is built

--- jar-build.sh
+++ jar-build.sh
@@ -2,13 +2,17 @@
 #set -x
 
 cwd="`pwd`"
-srcdir="${cwd}/src/main/java/com/fourjs/jsonhelper"
-javafile="${srcdir}/BDLJsonPath.java"
+rootdir="${cwd}/src/main/java"
+reldir="com/fourjs/jsonhelper"
+javafile="${reldir}/BDLJsonPath.java"
 
-if [ ! -d "$srcdir" ]; then
+srcdir="${cwd}/src/main/java/com/fourjs/jsonhelper"
+
+if [ ! -d "$rootdir" ]; then
    echo "You should be in the root git directory when running this script"
    exit 1
 fi
+cd $rootdir
 
 if [ ! -f "$javafile" ]; then
    echo "The java source file $javafile does not exist"
@@ -16,27 +20,25 @@ if [ ! -f "$javafile" ]; then
 fi
 
 javac "$javafile"
-
 if [ $? -ne 0 ]; then
    echo "Compilation error occurred"
    exit 1
 fi
 
-classfile="${srcdir}/BDLJsonPath.class"
+classfile="${reldir}/BDLJsonPath.class"
 if [ ! -f "$classfile" ]; then
    echo "Compilation error occurred"
    exit 1
 fi
 
-jarroot="${cwd}/src/main/java"
-if [ ! -d "$jarroot" ]; then
-   echo "You should be in the root git directory when running this script"
+echo Running test....
+java com.fourjs.jsonhelper.BDLJsonPath
+if [ $? -ne 0 ]; then
+   echo "BDLJsonPath test failed, please review the error(s)"
    exit 1
 fi
 
-cd "$jarroot"
-jar cMf json-path-helper.jar META-INF/MANIFEST.MF com/fourjs/jsonhelper/BDLJsonPath.class
-
+jar cMf json-path-helper.jar META-INF/MANIFEST.MF ${reldir}/BDLJsonPath.class
 if [ $? -ne 0 ]; then
    echo "An error occurred attempting to create the jar file"
    exit 1
